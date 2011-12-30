44a0aa2c1a25ebabda7a1f2ff3f53bad9711ec36
Chase Hutchins <syntaqx@gmail.com>
2011-12-30 03:14:59 -0700

    separated the bootstrap and autoload processes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -37,24 +37,8 @@
  */
 \date_default_timezone_set('UTC');
 
-// Include functionality we cannot autoload
-require_once __DIR__.'/../vendor/boilerplate/src/Boilerplate/Component/Autoloader/Universal.php';
-
-// Intantiate the Autoloader
-$loader = new Boilerplate\Component\Autoloader\Universal;
-
-// Register namespaced classes
-$loader->registerNamespaces(array(
-	'Boilerplate' => __DIR__.'/../vendor/boilerplate/src',
-));
-
-// Register namespace fallbacks
-$loader->registerNamespaceFallbacks(array(
-	__DIR__.'/src',
-));
-
-// Register the Autoloader to the SPL autoload stack
-$loader->register();
+// Bootstrap the Autoload process
+require_once __DIR__.'/autoload.php';
 
 /**
  * Sets the function that should be called in the event an exception is not
